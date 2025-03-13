f4176c9377f472195049370ada7df0f1ec31d2dd
RyanKoech <sirryankoech@gmail.com>
2025-03-13 19:23:02 +0300

    clone and copy

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,6 +1,10 @@
 mod request;
 mod method;
 mod query_string;
+mod response;
+mod status_code;
 
 pub use request::Request;
 pub use query_string::{QueryString};
+pub use response::Response;
+pub use status_code::StatusCode;
