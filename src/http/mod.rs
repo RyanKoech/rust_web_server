6c68bcc7c583aaa2b2bfb36f2abbb4fa9a08f118
RyanKoech <sirryankoech@gmail.com>
2025-03-13 18:27:26 +0300

    clean up

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,5 +2,3 @@ mod request;
 mod method;
 
 pub use request::Request;
-pub use method::Method;
-pub use request::ParseError;
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod server;
 mod http;
 
 use server::Server;
-use http::Request;
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
