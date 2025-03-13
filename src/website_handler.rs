d91d16cbe2420a017216b9acec5cda6018f8fbc7
RyanKoech <sirryankoech@gmail.com>
2025-03-13 20:58:48 +0300

    read and server content of file

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,6 +1,6 @@
 use crate::server::Handler;
 use crate::http::{Request, Response, StatusCode, Method};
-
+use std::fs;
 
 pub struct WebsiteHandler {
   public_path: String,
@@ -10,13 +10,18 @@ impl WebsiteHandler {
   pub fn new(public_path: String) -> Self {
       Self { public_path }
   }
+
+  fn read_file(&self, file_path: &str) -> Option<String> {
+    let path = format!("{}/{}", self.public_path, file_path);
+    fs::read_to_string(path).ok()
+  }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> crate::http::Response {
       match request.method() {
         Method::GET => match request.path() {
-            "/" => Response::new(StatusCode::Ok, Some("<h1> Hello From Rust Server!! </h1>".to_string())),
+            "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
             _ => Response::new(StatusCode::NotFound, None),
         },
         _ => Response::new(StatusCode::NotFound, None),
